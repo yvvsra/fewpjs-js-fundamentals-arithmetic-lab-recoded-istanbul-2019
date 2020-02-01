c63a7dcf2bebd0ccb2e9e1cebf9f6cac3c4b9f4b
yvvsra <yvvsra1@gmail.com>
2020-01-31 16:28:48 -0800

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-
 // Write your code here
+
+//function multiply(num1,num2){
+let num1=2;
+let num2=31;
+let multiply=num1*num2;
+//}
+
+//random function - random int > 0
+//function random(numBer){
+let random=Math.random();
+
+//remainder function- generates a remainder that is equal to 4
+let x=64;
+let y=15;
+let mod= x%y;
+
+//max function - will return 20 as the highest number in the set
+let max=Math.max(4,5,12,20);
